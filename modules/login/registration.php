e761efca692f82fb1c93216ed260f346cdcc13de
Алексей Иванов <Alessio18911@gmail.com>
2020-05-11 20:27:26 +0300

    Проверка на существующий email

--- modules/login/registration.php
+++ modules/login/registration.php
@@ -1,18 +1,31 @@
 <?php 
-// echo "<pre><div>";
-// print_r($_POST);
-// echo "</div></pre>";
 
 $pageTitle = "Регистрация";
 
 if (isset($_POST['register'])) {
-
     if (!trim($_POST['email'])) {
         $errors[] = ['title' => "Введите email", 'desc' => "<p>Email обязателен для регистрации на сайте</p>"];
+    } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors[] = ['title' => "Введите корректный email"];
     }
     
     if (!trim($_POST['password'])) {
         $errors[] = ['title' => "Введите пароль"];
+    }    
+
+    if (R::count('users', 'email = ?', array($_POST['email']))) {
+        $errors[] = [
+            'title' => "Данный email уже зарегистрирован",
+            'desc' => '<p>Используйте другой email или воспользуйтесь <a href="' .HOST. 'lost-password" ?>восстановлением пароля</a></p>'
+        ];
+    }
+    
+    if (empty($errors)) {
+        $user = R::dispense('users');
+        $user->email = $_POST['email'];
+        $user->role = 'user';
+        $user->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        R::store($user);
     }
 
 }
