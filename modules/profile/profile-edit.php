34d196dc195324b1d298b2252e7c7a6d8d766d93
Алексей Иванов <Alessio18911@gmail.com>
2020-05-29 23:32:42 +0300

    Редактирование профиля

--- modules/profile/profile-edit.php
+++ modules/profile/profile-edit.php
@@ -1,36 +1,53 @@
 <?php
 
+function updateUserAndGoToProfile($user) {
+  if (isset($_POST['update-profile'])) {
+
+    global $errors;
+
+    if (trim($_POST['name']) === '') {
+      $errors[] = ['title' => 'Введите имя'];
+    }
+
+    if (trim($_POST['surname']) === '') {
+      $errors[] = ['title' => 'Введите фамилию'];
+    }
+
+    if (trim($_POST['email']) === '') {
+      $errors[] = ['title' => 'Введите Email'];
+    }
+
+    if (empty($errors)) {
+      $user->name = htmlentities($_POST['name']);
+      $user->surname = htmlentities($_POST['surname']);
+      $user->email = htmlentities($_POST['email']);
+      $user->city = htmlentities($_POST['city']);
+      $user->country = htmlentities($_POST['country']);
+
+      R::store($user);
+      $_SESSION['logged_user']['id'] = $user;
+      header("Location: " .HOST. "profile");
+      exit();
+    }
+  }
+}
+
 if (isset($_SESSION['login']) && $_SESSION['login'] === 1) {
   if ($_SESSION['logged_user']['role'] === 'user') {
     $user = R::load('users', $_SESSION['logged_user']['id']);
 
-    if (isset($_POST['update-profile'])) {
-      if (trim($_POST['name']) === '') {
-        $errors[] = ['title' => 'Введите имя'];
-      }
+    updateUserAndGoToProfile($user);
 
-      if (trim($_POST['surname']) === '') {
-        $errors[] = ['title' => 'Введите фамилию'];
-      }
+  } elseif ($_SESSION['logged_user']['role'] === 'admin') {
+    if (isset($uriArray[1])) {
+      $user = R::load('users', (int)$uriArray[1]);
 
-      if (trim($_POST['email']) === '') {
-        $errors[] = ['title' => 'Введите Email'];
-      }
+      updateUserAndGoToProfile($user);
+    } else {
+      $user = R::load('users', $_SESSION['logged_user']['id']);
 
-      if (empty($errors)) {
-        $user->name = htmlentities($_POST['name']);
-        $user->surname = htmlentities($_POST['surname']);
-        $user->email = htmlentities($_POST['email']);
-        $user->city = htmlentities($_POST['city']);
-        $user->country = htmlentities($_POST['country']);
-
-        R::store($user);
-        $_SESSION['logged_user']['id'] = $user;
-        header("Location: " .HOST. "profile");
-        exit();
-      }
+      updateUserAndGoToProfile($user);
     }
-
   }
 } else {
   header("Location: " .HOST. "login");
