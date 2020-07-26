c0c473d8952e49ad198f54c1559c2ce52ea6012d
Алексей Иванов <Alessio18911@gmail.com>
2020-07-26 23:19:25 +0300

    Вывод всех постов на странице админки и блога

--- modules/blog/index.php
+++ modules/blog/index.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
 
-echo "<h1>Blog/index.php</h1>"
+$pageTitle = "Блог - все записи";
+$posts =  R::find('posts', 'ORDER BY id DESC');
+
+include ROOT . "templates/_page-parts/_head.tpl";
+include ROOT . "templates/_parts/_header.tpl";
+include ROOT . "templates/blog/index.tpl";
+include ROOT . "templates/_parts/_footer.tpl";
+include ROOT . "templates/_page-parts/_foot.tpl";
+
 ?>
