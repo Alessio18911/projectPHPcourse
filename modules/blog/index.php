59787d7598a93a0f2b8d08b340ca6eeaf1f776a0
Алексей Иванов <Alessio18911@gmail.com>
2020-07-27 14:48:21 +0300

    Вывод отдельного поста

--- modules/blog/index.php
+++ modules/blog/index.php
@@ -1,7 +1,23 @@
 <?php
 
 $pageTitle = "Блог - все записи";
-$posts =  R::find('posts', 'ORDER BY id DESC');
+
+if (isset($uriGet)) {
+  $post = R::load('posts', $uriGet);
+
+  ob_start();
+  include ROOT . "templates/blog/single-post.tpl";
+  $content = ob_get_contents();
+  ob_end_clean();
+
+} else {
+  $posts =  R::find('posts', 'ORDER BY id DESC');
+
+  ob_start();
+  include ROOT . "templates/blog/all-posts.tpl";
+  $content = ob_get_contents();
+  ob_end_clean();
+}
 
 include ROOT . "templates/_page-parts/_head.tpl";
 include ROOT . "templates/_parts/_header.tpl";
