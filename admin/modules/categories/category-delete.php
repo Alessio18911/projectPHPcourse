775dda8c040593192bbf46df4fbc369853dfc29e
Алексей Иванов <Alessio18911@gmail.com>
2020-09-27 13:46:40 +0300

    Category deletion done

--- admin/modules/categories/category-delete.php
+++ admin/modules/categories/category-delete.php
@@ -1,6 +1,16 @@
 <?php
 
-$category_to_delete_name = '';
+$category_id = $_GET['id'];
+$category_to_delete = R::findOne('categories', 'id=?', [$category_id]);
+$category_to_delete_name = $category_to_delete->name;
+
+if (isset($_POST['delete-category'])) {
+  R::trash($category_to_delete);
+
+  $_SESSION['success']['category_deleted'][] = "success";
+  header("Location: " .HOST. "admin/categories");
+  exit();
+}
 
 ob_start();
 include ROOT . "admin/templates/categories/category-delete.tpl";
