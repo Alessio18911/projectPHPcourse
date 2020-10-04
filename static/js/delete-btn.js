155a0d517e9bef1aa398cc0bce943a1584c69249
Алексей Иванов <Alessio18911@gmail.com>
2020-10-04 21:50:14 +0300

    Fixies delete-btn.js

--- static/js/delete-btn.js
+++ static/js/delete-btn.js
@@ -1,18 +1,17 @@
 const coverDeleteBtn = document.querySelector('#delete-cover');
 const avatarDeleteBtn = document.querySelector('#delete-avatar');
 
-changeLabelText(coverDeleteBtn);
-changeLabelText(avatarDeleteBtn);
-
-const onDeleteBtnChange = function onDeleteBtnChange(evt) {
-  const deleteLabel = evt.target.nextElementSibling;
-  deleteLabel.textContent = !evt.target.checked ? "Удалить фотографию" : "Отменить удаление фотографии";
+function changeLabelText(button) {
+  button.addEventListener('change', function (evt) {
+    const button = evt.target;
+    button.nextElementSibling.textContent = !button.checked ? "Удалить фотографию" : "Отменить удаление фотографии";
+  });
 }
 
-const changeLabelText = function(button) {
-  if (button) {
-    button.addEventListener('change', onDeleteBtnChange);
-  }
+if (coverDeleteBtn) {
+  changeLabelText(coverDeleteBtn);
 }
 
-
+if (avatarDeleteBtn) {
+  changeLabelText(avatarDeleteBtn);
+}
