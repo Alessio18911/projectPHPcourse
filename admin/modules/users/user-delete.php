ba56723d0ed1a0bede088d2a4b7ebd48cdf66e74
Алексей Иванов <Alessio18911@gmail.com>
2021-02-21 12:57:54 +0300

    User deletion module with avatar deletion done

--- admin/modules/users/user-delete.php
+++ admin/modules/users/user-delete.php
@@ -5,10 +5,17 @@ $user_to_delete = R::findOne('users', 'id=?', [$user_id]);
 $user_to_delete_name = $user_to_delete->name;
 $user_to_delete_surname = $user_to_delete->surname;
 $user_role = $user_to_delete->role;
+$user_to_delete_avatar = $user_to_delete->avatar;
+$avatar_path = ROOT . "usercontent/avatars/";
+$avatar_name_prefix = "48-";
 
 if (isset($_POST['delete-user'])) {
   if ($user_role != 'admin') {
     $deletion_result = R::trash($user_to_delete);
+
+    if (isset($user_to_delete_avatar)) {
+      delete_file($avatar_path, $user_to_delete_avatar, $avatar_name_prefix);
+    }
 
     if (!$deletion_result) {
       $_SESSION['errors']['user_deleted'][] = "failed";
